9157965f0788625d465811418afe35528994f542
CarlosDC <carlosdcorrea1999@hotmail.com>
2020-03-09 15:21:48 -0500

    programamos el intent en ambos botones del Loggin

--- pets/app/src/main/java/com/example/pets/LoginActivity.java
+++ pets/app/src/main/java/com/example/pets/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.pets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -15,6 +16,20 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void btn_Loggin(View view){
-        Toast.makeText(this, "it has been clicked", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "you are loggin in", Toast.LENGTH_LONG).show();
+
+        Intent intent = new Intent( this,
+                    inbox_activity.class);
+        startActivity(intent);
+    }
+
+
+
+
+    public void btn_sign_up(View view){
+        Toast.makeText(this, "Go to register page", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent( this,
+                register.class);
+        startActivity(intent);
     }
 }

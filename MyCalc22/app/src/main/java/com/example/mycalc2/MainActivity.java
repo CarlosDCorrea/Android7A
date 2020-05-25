ae5bed2c5ae52995b89428faf2fdecd1c2e618dd
CarlosDCorrea <carlosdcorrea1999@hotmail.com>
2020-05-25 13:40:15 -0500

    mycalc22

--- MyCalc22/app/src/main/java/com/example/mycalc2/MainActivity.java
+++ MyCalc22/app/src/main/java/com/example/mycalc2/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.mycalc2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        output = findViewById(R.id.lblOutput);
     }
+
+    public void btn1 (View view){
+        String number = output.getText().toString();
+        output.setText(number);
+    }
+
 }
